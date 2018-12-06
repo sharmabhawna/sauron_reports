d1bcf26292b8f578fe9a2ffd5fe5a9c442ec5299
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-12-06 19:51:23 +0530

    Fixing loading of css issue. Slack notification

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   pathPrefix: '/sauron_reports',
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
