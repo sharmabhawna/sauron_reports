427e19c494f1fbfb573deb408a533d23def4ee13
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-12-29 16:17:14 +0530

    Setting the timeline domain to view commits

--- src/specs/commit.js
+++ src/specs/commit.js
@@ -1,33 +1,40 @@
-export default function() {return {
-  $schema: 'https://vega.github.io/schema/vega-lite/v3.0.0-rc10.json',
-  description: 'A simple bar chart with embedded data.',
-  data: {
-    values:[],
-  },
-  config: {
-    "style": {
-      "cell": {
-        "stroke": "transparent"
+export default function() {
+  return {
+    $schema: 'https://vega.github.io/schema/vega-lite/v3.0.0-rc10.json',
+    description: 'A simple bar chart with embedded data.',
+    data: {
+      values: []
+    },
+    config: {
+      style: {
+        cell: {
+          stroke: 'transparent'
+        }
+      },
+      padding: null,
+      line: {
+        strokeWidth: 0.8,
+        interpolate: 'basis'
       }
     },
-    "padding": null,
-    line: {
-      strokeWidth: 0.8,
-      interpolate: "basis"
-    }
-  },
-  width: 100,
-  height: 10,
-  mark: 'tick',
+    width: 100,
+    height: 10,
+    mark: 'tick',
 
-  encoding: {
-    x: {
-      field: 'commit.timestamp',
-      type: 'temporal',
-      order: null,
-      axis: null
-    },
-    order: null,
-  }
-}
+    encoding: {
+      x: {
+        field: 'commit.timestamp',
+        type: 'temporal',
+        order: null,
+        axis: null,
+        scale: {
+          domain: [
+            new Date('December 26,2018, 09:00:00').toISOString(),
+            new Date().toISOString()
+          ]
+        }
+      },
+      order: null
+    }
+  };
 }
